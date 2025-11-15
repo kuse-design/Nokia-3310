6cd7812c75b579d93acd64fabb1722be149da3ff
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-15 14:32:36 +0100

    Nokia 3310

--- NokiaPhone.java
+++ NokiaPhone.java
@@ -104,8 +104,7 @@ switch (menu){
 
         } break;
     
-    case 3: System.out.println(" Chat ");
-
+    case 3: System.out.println(" chat  ");
     
     case 4: 
         System.out.println("  Call register ");
@@ -125,32 +124,117 @@ switch (menu){
         int moreOptions;
         moreOptions = input.nextInt();
 
-//        if(moreOption == 5){
-//            System.out.println(" 1 Last call duration ");
-//            System.out.println(" 2 All calls duration ");
-//            System.out.println(" 3 Received calls duration ");
-//            System.out.println(" 4 Dialled calls dueration ");
-//            System.out.println(" 5 Clear timers ");
-//
-//        }
-//        if else{
-//            System.out.println(" invalid number ");
-//        }
-//
-//       if else(moreOption == 6){
-//            System.out.println(" 1 Last call cost ");
-//            System.out.println(" 2 All calls cost ");
-//            System.out.println(" 3 Clear coumters ");
-//          }
-//        if else(moreOption == 7){
-//            System.out.println(" 1 Call cost settings ");
-//            System.out.println(" 2 Show cost limit ");
-//        else{
-//            System.out.println(" invalid number ");
-//        }
+        if(moreOptions == 5){
+            System.out.println(" 1 Last call duration ");
+            System.out.println(" 2 All calls duration ");
+            System.out.println(" 3 Received calls duration ");
+            System.out.println(" 4 Dialled calls dueration ");
+            System.out.println(" 5 Clear timers ");
+
+        }
+
+       else if(moreOptions == 6){
+            System.out.println(" 1 Last call cost ");
+            System.out.println(" 2 All calls cost ");
+            System.out.println(" 3 Clear coumters ");
+          }
+        else if(moreOptions == 7){
+            System.out.println(" 1 Call cost settings ");
+            System.out.println(" 2 Show cost limit ");
+            }
+
+        else{
+            System.out.println(" invalid number ");
+        }
+        
+        
+         case 5: 
+        System.out.println("  Tones ");
+        System.out.println(" 1 Ringing tone ");
+        System.out.println(" 2 Ringing volume ");
+        System.out.println(" 3 Incoming call alert");
+        System.out.println(" 4 Composer ");
+        System.out.println(" 5 Message alert tone ");
+        System.out.println(" 6 Keypad tones ");
+        System.out.println(" 7 Warming and game tones ");
+        System.out.println(" 8 Vibrating alert");
+        System.out.println(" 9 Screen saver ");
+
+
+        case 6:
+
+        System.out.println(" Settings ");
+        System.out.println(" 1 Call settings ");
+        System.out.println(" 2 Phone setting ");
+        System.out.println(" 3 Security settings ");
+        System.out.println(" 4 Restore factory settings ");
+        System.out.println(" Enter 1 or 2 or 3 or 4 for more options");
+
+        int viewOption;
+        viewOption = input.nextInt();
+
+        if (viewOption == 1){
+        System.out.println(" 1 Automatic redial ");
+        System.out.println(" 2 Speed dailing ");
+        System.out.println(" 3 Call waiting options ");
+        System.out.println(" 4 Own number sending ");
+        System.out.println(" 5 Phone line in use ");
+        System.out.println(" 6 Automatic answer ");
+
+        }
+
+        else if(viewOption == 2){
+        System.out.println(" 1 Language ");
+        System.out.println(" 2 Cell info display ");
+        System.out.println(" 3 Welcome note ");
+        System.out.println(" 4 Network selection ");
+        System.out.println(" 5 Light ");
+        System.out.println(" 6 Comfirm SIM service actions ");
+
+        }
+
+        else if(viewOption == 3){
+        System.out.println(" 1 PIN code request ");
+        System.out.println(" 2 Call barring service ");
+        System.out.println(" 3 Fixed dailing ");
+        System.out.println(" 4 Closed user group ");
+        System.out.println(" 5 Phone security ");
+        System.out.println(" 6 Change access codes ");
+
+        }
+
+        else{
+        System.out.println(" invalid number ");
+
+        }
+
+
+         case 7: System.out.println(" Call divert  ");
+
+         case 8: System.out.println(" Games  ");
+
+         case 9: System.out.println(" Calculator  ");
+
+         case 10: System.out.println(" Reminder  ");
+
+         case 11: 
+            
+         System.out.println("  Clock ");
+        System.out.println(" 1 Alarm clock ");
+        System.out.println(" 2 Clock settings ");
+        System.out.println(" 3 Date setting");
+        System.out.println(" 4 Stopwatch ");
+        System.out.println(" 5 Countdown timer ");
+        System.out.println(" 6 Auto update of date and time ");
+
+
+
+        case 12: System.out.println(" Profile ");
         
 
-           
+        case 13: System.out.println(" SIM service ");
+
+
 
 }
 }

44b09438c5e233f4d651db922f8bc2def02bf86d
Kuse_design <0luwakusejerry1@gmail.com>
2025-11-15 22:15:22 +0100

    NokiaPhone

--- NokiaPhone.java
+++ NokiaPhone.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class NokiaPhone{
+public class PhoneApp{
 public static void main(String[]args){
 
 Scanner input = new Scanner(System.in);
@@ -39,21 +39,21 @@ switch (menu){
         System.out.println(" 8 Options ");
         System.out.println(" 9 Speed ");
         System.out.println(" 10 voice tags ");
+        System.out.println(".............................................");
         System.out.println(" Enter number 8 for more Options :");
 
 
-        int option;
-        option = input.nextInt();
-        if(option == 8){
+        int option = input.nextInt();
+        switch (option){
+        case 8:
             System.out.println(" 1 Type of view");
             System.out.println(" 2 Memory status ");
-        }
-        else{
-            System.out.println("Invalid number");
+        break;
 
-        }
-
-    
+        default : System.out.println("Select one of the above option");
+         } break;
+            
+        
     case 2:
 
         System.out.println(" Message ; ");
@@ -67,45 +67,48 @@ switch (menu){
         System.out.println(" 8 Info service ");
         System.out.println(" 9 Voice mailbox number ");
         System.out.println(" 10 Service command editor ");
-
+        System.out.println(".............................................");
         System.out.print("Enter number 7 for more options in Message Settings: ");
 
-        int messageSettings;
-        messageSettings = input.nextInt();
-        if(messageSettings == 7){
+      
+       int messageSettings = input.nextInt();
+
+        switch (messageSettings){
+        case 7:
+ 
             System.out.println(" 1 Set 1");
             System.out.println(" 2 Common ");
-            System.out.println(" Enter number 1 or 2 for more options : ");
+        break;
+
+        default : System.out.println(" Enter number 1 or 2 for more options : ");
         }
-        else{
-            System.out.println(" invalid number ");
-        }
 
-        int messageSettingsMoreOptions = input.nextInt();;
+            int settingsMoreOptions = input.nextInt();
 
 
 
-        switch(messageSettingsMoreOptions){
+            switch (settingsMoreOptions){
 
-        case 1: 
-      
-            System.out.println(" 1 Message centre number ");
-            System.out.println(" 2 Message sent as ");
-            System.out.println(" 3 Message settings "); 
+            case 1: 
+          
+                System.out.println(" 1 Message centre number ");
+                System.out.println(" 2 Message sent as ");
+                System.out.println(" 3 Message settings "); 
 
-            break;
+                break;
 
 
-        case 2: 
+            case 2: 
+            
+                System.out.println(" 1 Delivery report ");
+                System.out.println(" 2 Reply via same centre ");
+                System.out.println(" 3 Character support ");
+
+            } break;
         
-            System.out.println(" 1 Delivery report ");
-            System.out.println(" 2 Reply via same centre ");
-            System.out.println(" 3 Character support ");
-
-        } break;
-    
     case 3: System.out.println(" chat  ");
-    
+
+             break;
     case 4: 
         System.out.println("  Call register ");
         System.out.println(" 1 Missed calls ");
@@ -116,39 +119,41 @@ switch (menu){
         System.out.println(" 6 Show call cost ");
         System.out.println(" 7 Call cost settings ");
         System.out.println(" 8 Prepaid credit ");
-
+        System.out.println(".............................................");
         System.out.println(" Enter number 5 or 6 or 7 for more option : ");
         
 
 
-        int moreOptions;
-        moreOptions = input.nextInt();
+        int moreOptions = input.nextInt();
 
-        if(moreOptions == 5){
-            System.out.println(" 1 Last call duration ");
-            System.out.println(" 2 All calls duration ");
-            System.out.println(" 3 Received calls duration ");
-            System.out.println(" 4 Dialled calls dueration ");
-            System.out.println(" 5 Clear timers ");
+            switch (moreOptions){
+                case 5:
+                    System.out.println(" 1 Last call duration ");
+                    System.out.println(" 2 All calls duration ");
+                    System.out.println(" 3 Received calls duration ");
+                    System.out.println(" 4 Dialled calls dueration ");
+                    System.out.println(" 5 Clear timers ");
 
-        }
+                     break;                
 
-       else if(moreOptions == 6){
-            System.out.println(" 1 Last call cost ");
-            System.out.println(" 2 All calls cost ");
-            System.out.println(" 3 Clear coumters ");
-          }
-        else if(moreOptions == 7){
-            System.out.println(" 1 Call cost settings ");
-            System.out.println(" 2 Show cost limit ");
-            }
+              
+               case 6:
+                    System.out.println(" 1 Last call cost ");
+                    System.out.println(" 2 All calls cost ");
+                    System.out.println(" 3 Clear coumters ");
+                  
+                     break;
+                
+                case 7:
+                    System.out.println(" 1 Call cost settings ");
+                    System.out.println(" 2 Show cost limit ");
 
-        else{
-            System.out.println(" invalid number ");
-        }
+                }
+
+                break;
         
         
-         case 5: 
+    case 5: 
         System.out.println("  Tones ");
         System.out.println(" 1 Ringing tone ");
         System.out.println(" 2 Ringing volume ");
@@ -160,65 +165,75 @@ switch (menu){
         System.out.println(" 8 Vibrating alert");
         System.out.println(" 9 Screen saver ");
 
+        
+             break;
 
-        case 6:
+    case 6:
 
         System.out.println(" Settings ");
         System.out.println(" 1 Call settings ");
         System.out.println(" 2 Phone setting ");
         System.out.println(" 3 Security settings ");
         System.out.println(" 4 Restore factory settings ");
+        System.out.println(".............................................");
         System.out.println(" Enter 1 or 2 or 3 or 4 for more options");
 
-        int viewOption;
-        viewOption = input.nextInt();
+       int viewOption = input.nextInt();
 
-        if (viewOption == 1){
-        System.out.println(" 1 Automatic redial ");
-        System.out.println(" 2 Speed dailing ");
-        System.out.println(" 3 Call waiting options ");
-        System.out.println(" 4 Own number sending ");
-        System.out.println(" 5 Phone line in use ");
-        System.out.println(" 6 Automatic answer ");
+            switch (viewOption){
 
-        }
+            case 1:
+            System.out.println(" 1 Automatic redial ");
+            System.out.println(" 2 Speed dailing ");
+            System.out.println(" 3 Call waiting options ");
+            System.out.println(" 4 Own number sending ");
+            System.out.println(" 5 Phone line in use ");
+            System.out.println(" 6 Automatic answer ");
 
-        else if(viewOption == 2){
-        System.out.println(" 1 Language ");
-        System.out.println(" 2 Cell info display ");
-        System.out.println(" 3 Welcome note ");
-        System.out.println(" 4 Network selection ");
-        System.out.println(" 5 Light ");
-        System.out.println(" 6 Comfirm SIM service actions ");
+             break;            
 
-        }
-
-        else if(viewOption == 3){
-        System.out.println(" 1 PIN code request ");
-        System.out.println(" 2 Call barring service ");
-        System.out.println(" 3 Fixed dailing ");
-        System.out.println(" 4 Closed user group ");
-        System.out.println(" 5 Phone security ");
-        System.out.println(" 6 Change access codes ");
-
-        }
-
-        else{
-        System.out.println(" invalid number ");
-
-        }
-
-
-         case 7: System.out.println(" Call divert  ");
-
-         case 8: System.out.println(" Games  ");
-
-         case 9: System.out.println(" Calculator  ");
-
-         case 10: System.out.println(" Reminder  ");
-
-         case 11: 
+            case 2:
+            System.out.println(" 1 Language "); 
+            System.out.println(" 2 Cell info display ");
+            System.out.println(" 3 Welcome note ");
+            System.out.println(" 4 Network selection ");
+            System.out.println(" 5 Light ");
+            System.out.println(" 6 Comfirm SIM service actions ");
             
+            break;
+
+            
+
+            case 3:
+            System.out.println(" 1 PIN code request ");
+            System.out.println(" 2 Call barring service ");
+            System.out.println(" 3 Fixed dailing ");
+            System.out.println(" 4 Closed user group ");
+            System.out.println(" 5 Phone security ");
+            System.out.println(" 6 Change access codes ");
+
+            
+            }
+            break;
+
+     case 7: System.out.println(" Call divert  ");
+
+            break;
+
+     case 8: System.out.println(" Games  ");
+
+            break;
+
+     case 9: System.out.println(" Calculator  ");
+
+            break;
+
+     case 10: System.out.println(" Reminder  ");
+
+            break;
+
+     case 11: 
+        
          System.out.println("  Clock ");
         System.out.println(" 1 Alarm clock ");
         System.out.println(" 2 Clock settings ");
@@ -227,68 +242,19 @@ switch (menu){
         System.out.println(" 5 Countdown timer ");
         System.out.println(" 6 Auto update of date and time ");
 
+             break;
 
+    case 12: System.out.println(" Profile ");
+    
+             break;
 
-        case 12: System.out.println(" Profile ");
-        
-
-        case 13: System.out.println(" SIM service ");
-
-
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    case 13: System.out.println(" SIM service ");
+    }
 
 
 }
+}
+
 
 
 
